04fb580135d7f34f7f1c6e626c5fb27535c07bc6
BarzuVanessa <barzuvanessa@gmail.com>
2025-04-14 17:01:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function openALLARTICLES () {
+    window.location.href= 'index.html';
+ }
+ 
+ 
+ function openCONTACT(){
+     window.location.href ='contact.html';
+ }
+ 
+ 
+ function openREVIEWS () {
+    window.location.href='reviews.html';
+ }
+
+
+ function openEXPERIENCE (){
+   window.location.href='EXPERIENCE.html'
+ }
